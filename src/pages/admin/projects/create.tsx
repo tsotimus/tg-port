5d88aeb51451b8de677c52d72fe016ec98613d57
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-03-29 23:59:28 +0000

    add titles to pages

--- src/pages/admin/projects/create.tsx
+++ src/pages/admin/projects/create.tsx
@@ -1,10 +1,14 @@
 import AdminLayout from "@/components/layouts/AdminLayout";
 import Stack from "@/components/layouts/Stack";
 import CreateProject from "@/features/Admin/Projects/CreateProject";
+import Head from "next/head";
 
 const CreateProjectPage = () => {
   return (
     <AdminLayout>
+      <Head>
+        <title>Create Project</title>
+      </Head>
       <Stack className="p-12">
         <CreateProject />
       </Stack>
--- src/pages/admin/projects/index.tsx
+++ src/pages/admin/projects/index.tsx
@@ -3,11 +3,15 @@ import Stack from "@/components/layouts/Stack";
 import Typography from "@/components/typography/Typography";
 import { Button } from "@/components/ui/button";
 import MyProjects from "@/features/Admin/Projects/MyProjects";
+import Head from "next/head";
 import Link from "next/link";
 
 const MyProjectsPage = () => {
   return (
     <AdminLayout>
+      <Head>
+        <title>My Projects</title>
+      </Head>
       <Stack justify="center" align="center" className="pt-6" gap={8}>
         <Typography variant="h1">My Projects</Typography>
         <Link href="/admin/projects/create">
