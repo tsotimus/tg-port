9e98e6b6e37084f3ec888ba6f2515070ad3f9ad4
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-03-29 20:05:49 +0000

    fix callouts

--- src/components/mdx/Callout.tsx
+++ src/components/mdx/Callout.tsx
@@ -11,18 +11,10 @@ const TypeToEmoji = {
 type CalloutType = keyof typeof TypeToEmoji;
 
 const classes: Record<CalloutType, string> = {
-  default: cn(
-    "nx-border-orange-100 nx-bg-orange-50 nx-text-orange-800 dark:nx-border-orange-400/30 dark:nx-bg-orange-400/20 dark:nx-text-orange-300"
-  ),
-  error: cn(
-    "nx-border-red-200 nx-bg-red-100 nx-text-red-900 dark:nx-border-red-200/30 dark:nx-bg-red-900/30 dark:nx-text-red-200"
-  ),
-  info: cn(
-    "nx-border-blue-200 nx-bg-blue-100 nx-text-blue-900 dark:nx-border-blue-200/30 dark:nx-bg-blue-900/30 dark:nx-text-blue-200"
-  ),
-  warning: cn(
-    "nx-border-yellow-100 nx-bg-yellow-50 nx-text-yellow-900 dark:nx-border-yellow-200/30 dark:nx-bg-yellow-700/30 dark:nx-text-yellow-200"
-  ),
+  default: cn("border-orange-400/30 bg-orange-400/20 text-orange-300"),
+  error: cn("border-red-200/30 bg-red-900/30 text-red-200"),
+  info: cn("border-blue-200/30 bg-blue-900/30 text-blue-200"),
+  warning: cn("border-yellow-200/30 bg-yellow-700/30 text-yellow-200"),
 };
 
 type CalloutProps = {
@@ -39,13 +31,13 @@ export function Callout({
   return (
     <div
       className={cn(
-        "nextra-callout nx-overflow-x-auto nx-mt-6 nx-flex nx-rounded-lg nx-border nx-py-2 ltr:nx-pr-4 rtl:nx-pl-4",
-        "contrast-more:nx-border-current contrast-more:dark:nx-border-current",
+        "callout overflow-x-auto mt-6 flex rounded-lg border py-2 ltr:pr-4 rtl:pl-4",
+        "contrast-more:border-current contrast-more:border-current",
         classes[type]
       )}
     >
       <div
-        className="nx-select-none nx-text-xl ltr:nx-pl-3 ltr:nx-pr-2 rtl:nx-pr-3 rtl:nx-pl-2"
+        className="select-none text-xl ltr:pl-3 ltr:pr-2 rtl:pr-3 rtl:pl-2 px-2"
         style={{
           fontFamily:
             '"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
@@ -53,7 +45,7 @@ export function Callout({
       >
         {emoji}
       </div>
-      <div className="nx-w-full nx-min-w-0 nx-leading-7">{children}</div>
+      <div className="w-full min-w-0 leading-7">{children}</div>
     </div>
   );
 }
