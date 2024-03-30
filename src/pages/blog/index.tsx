5130aeb98bfa31978b4e8d29c4db512e64da6f37
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-03-30 15:10:56 +0000

    add pages

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import Typography from "@/components/typography/Typography";
+
+const AllBlogPostsPage = () => {
+  return (
+    <MainLayout>
+      <Typography variant="h1">All Blog Posts</Typography>
+    </MainLayout>
+  );
+};
+
+export default AllBlogPostsPage;
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import Typography from "@/components/typography/Typography";
+
+const AllProjectsPage = () => {
+  return (
+    <MainLayout>
+      <Typography variant="h1">All Projects</Typography>
+    </MainLayout>
+  );
+};
+
+export default AllProjectsPage;
