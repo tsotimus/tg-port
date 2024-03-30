6fa42f622d755a9ab9f042d81e8b482687f8912e
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-03-30 15:08:37 +0000

    update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import Stack from "@/components/layouts/Stack";
 import Intro from "@/features/Public/Home/Intro";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <MainLayout>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Stack>
         <Intro />
       </Stack>
