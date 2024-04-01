c3d56308c224642a19fde8815ef3e0547abc7909
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-04-01 23:53:11 +0100

    Dropzone working

--- src/components/form/DropzoneInput.tsx
+++ src/components/form/DropzoneInput.tsx
@@ -1,15 +1,91 @@
 import { Controller, RegisterOptions, useFormContext } from "react-hook-form";
-import DropzoneField from "./Dropzone";
+import {
+  Accept,
+  useDropzone,
+  FileRejection,
+  FileWithPath,
+} from "react-dropzone";
+import { useMemo } from "react";
+import Typography from "../typography/Typography";
+import { FileIcon } from "@radix-ui/react-icons";
+
+const baseClassNames =
+  "flex flex-col items-center w-3/4 m-4 p-20 outline-none transition-border duration-200 ease-in-out cursor-pointer border-2 rounded border-gray-200";
 
 interface DropzoneProps {
   name: string;
   rules: RegisterOptions;
-  acceptedFiles?: string[];
+  allowedFiles: Accept;
+  maxFiles?: number;
+  maxSize?: number;
 }
-const DropzoneInput = ({ name, rules }: DropzoneProps) => {
-  const { control } = useFormContext();
 
-  const handleDrop = (acceptedFiles: File[]) => {};
+interface FileRejectionWithPath extends FileRejection {
+  path: string;
+}
+
+const DropzoneInput = ({
+  name,
+  rules,
+  maxFiles,
+  maxSize,
+  allowedFiles,
+}: DropzoneProps) => {
+  const { control, setValue } = useFormContext();
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+    isFocused,
+    acceptedFiles,
+    fileRejections,
+  } = useDropzone({
+    accept: allowedFiles,
+    maxFiles,
+    maxSize,
+    onDrop: (acceptedFiles) => {
+      setValue(name, acceptedFiles as unknown as FileList, {
+        shouldValidate: true,
+      });
+    },
+  });
+
+  const acceptedFileItems = (acceptedFiles as FileWithPath[]).map((file) => (
+    <li key={file.path} className="flex gap-4">
+      <FileIcon />
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => {
+    const typedFile = file as FileWithPath;
+    return (
+      <li key={typedFile.path}>
+        {typedFile.path} - {file.size} bytes
+        <ul>
+          {errors.map((e) => (
+            <li key={e.code}>{e.message}</li>
+          ))}
+        </ul>
+      </li>
+    );
+  });
+
+  const additionalClass = useMemo(() => {
+    if (isFocused) {
+      return "border-blue-500";
+    }
+    if (isDragAccept) {
+      return "border-green-500";
+    }
+    if (isDragReject) {
+      return "border-red-500";
+    }
+    return "";
+  }, [isFocused, isDragAccept, isDragReject]);
 
   return (
     <Controller
@@ -17,7 +93,32 @@ const DropzoneInput = ({ name, rules }: DropzoneProps) => {
       rules={rules}
       control={control}
       render={({ field: { onChange, onBlur, value } }) => {
-        return <DropzoneField acceptedFiles={[]} />;
+        return (
+          <div className="w-full max-w-2xl flex-1 flex flex-col items-center border-2 rounded border-gray-200 bg-gray-100 text-gray-400">
+            <div
+              {...getRootProps({
+                className: `${baseClassNames} ${additionalClass}`,
+              })}
+            >
+              <input {...getInputProps({ onChange, onBlur })} />
+              {isDragAccept && (
+                <Typography>All files will be accepted</Typography>
+              )}
+              {isDragReject && (
+                <Typography>Some files will be rejected</Typography>
+              )}
+              {!isDragActive && (
+                <Typography>Drop some files here ...</Typography>
+              )}
+            </div>
+            <aside className="p-4 flex flex-col w-full">
+              <h4>Accepted files</h4>
+              <ul>{acceptedFileItems}</ul>
+              <h4>Rejected files</h4>
+              <ul>{fileRejectionItems}</ul>
+            </aside>
+          </div>
+        );
       }}
     />
   );
--- src/features/Admin/Media/UploadMediaForm.tsx
+++ src/features/Admin/Media/UploadMediaForm.tsx
@@ -1,15 +1,46 @@
 import DropzoneInput from "@/components/form/DropzoneInput";
 import Stack from "@/components/layouts/Stack";
+import { Button } from "@/components/ui/button";
 import { FormProvider, useForm } from "react-hook-form";
 
+const acceptedFiles = {
+  "image/*": [".png", ".gif", ".jpeg", ".jpg"],
+  "video/*": [".mp4", ".webm", ".ogg"],
+};
+
+type FormData = {
+  media: FileList;
+};
+
 const UploadMediaForm = () => {
-  const methods = useForm();
+  const methods = useForm<FormData>({
+    mode: "onChange",
+  });
+
+  const {
+    handleSubmit,
+    formState: { isValid },
+  } = methods;
+
+  const onSubmit = (data: FormData) => {
+    console.log("Here");
+    console.log(data);
+  };
 
   return (
     <FormProvider {...methods}>
-      <Stack component="form">
-        <DropzoneInput name="media" rules={{ required: "Media is required" }} />
-      </Stack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack className="items-center" gap={12}>
+          <DropzoneInput
+            name="media"
+            rules={{ required: "Media is required" }}
+            allowedFiles={acceptedFiles}
+          />
+          <Button type="submit" disabled={!isValid}>
+            Submit
+          </Button>
+        </Stack>
+      </form>
     </FormProvider>
   );
 };
