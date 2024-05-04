a9de488f454dbf6784bd49a180473e57aab45fd2
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-05-04 23:08:56 +0100

    chore: add prefix

--- src/utils/server/files/fileStream.ts
+++ src/utils/server/files/fileStream.ts
@@ -6,6 +6,8 @@ export type VolatileFileType = InstanceType<typeof VolatileFile>;
 
 export interface MyVolatileFile extends VolatileFileType, File {}
 
+const PREFIX = "assets/";
+
 export const createCloudinaryStreamForImage = (
   publicId: string | undefined
 ) => {
@@ -13,7 +15,7 @@ export const createCloudinaryStreamForImage = (
     throw new Error("public_id is required");
   }
   const cloudinaryStream = cloudinary.uploader.upload_stream(
-    { public_id: publicId },
+    { public_id: `${PREFIX}${publicId}` },
     (error, result) => {
       if (error) {
         console.error("Upload to Cloudinary failed:", error);
@@ -34,7 +36,7 @@ export const createCloudinaryStreamForVideo = (
   }
 
   const cloudinaryStream = cloudinary.uploader.upload_chunked_stream(
-    { public_id: publicId },
+    { public_id: `${PREFIX}${publicId}` },
     (error, result) => {
       if (error) {
         console.error("Upload to Cloudinary failed:", error);
