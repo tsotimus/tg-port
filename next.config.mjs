0791a02707264f9f24950dc81a54e3ccf98cac1a
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-05-07 00:54:54 +0100

    chore: feature flag construction works

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ["@mdxeditor/editor"],
-  reactStrictMode: true,
-  redirects: async () => {
-    return [
-      {
-        source: "/((?!construction).)*",
-        destination: "/construction",
-        permanent: false,
-      },
-    ];
-  },
+const generateConfig = async () => {
+  const IS_UNDER_CONSTRUCTION = process.env.IS_UNDER_CONSTRUCTION === "true";
+
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    transpilePackages: ["@mdxeditor/editor"],
+    reactStrictMode: true,
+    redirects: async () => {
+      return IS_UNDER_CONSTRUCTION
+        ? [
+            {
+              source: "/((?!construction).)*",
+              destination: "/construction",
+              permanent: false,
+            },
+          ]
+        : [];
+    },
+  };
+  return nextConfig;
 };
 
-export default nextConfig;
+export default generateConfig;
