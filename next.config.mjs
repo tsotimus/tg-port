edf13a913f293cac0fa9b28214c5de42a4eb08dc
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-05-07 00:47:19 +0100

    chore: add construction page and redirect@

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   transpilePackages: ["@mdxeditor/editor"],
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/((?!construction).)*",
+        destination: "/construction",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/pages/construction.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { CldImage } from "next-cloudinary";
+import Stack from "@/components/layouts/Stack";
+
+const ConstructionPage = () => {
+  return (
+    <Stack
+      gap={12}
+      className="bg-gray-100 min-h-screen items-center justify-center"
+    >
+      <CldImage
+        width="600"
+        height="600"
+        src="assets/important/construction-svg"
+        alt="Under construction illustration"
+      />
+      <h1 className="mb-4 text-3xl font-bold text-gray-800">
+        Pardon the Sawdust!
+      </h1>
+      <Stack gap={2} className="items-center justify-center">
+        <p className="mb-8 text-lg text-gray-600">
+          I am currently renovating and upgrading this website. It&apos;s like
+          moving furniture, but for the internet.
+        </p>
+        <p className="mb-8 text-lg text-gray-600">
+          In the meantime, you can check out my open source projects on GitHub.
+        </p>
+      </Stack>
+      <Button asChild>
+        <Link href="https://github.com/RockiRider">Visit my GitHub</Link>
+      </Button>
+    </Stack>
+  );
+};
+
+export default ConstructionPage;
