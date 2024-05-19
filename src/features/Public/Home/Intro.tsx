ffc30ff79e6162b638bdfa13e3a177469c85d245
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-05-19 17:07:12 +0100

    chore: addjust intro size

--- src/features/Public/Home/Intro.tsx
+++ src/features/Public/Home/Intro.tsx
@@ -9,7 +9,7 @@ const Intro = () => {
       className="bg-slate-900 dark:bg-background w-full"
       gap={20}
     >
-      <h1 className="sm:text-3xl md:text-2xl lg:text-5xl m-6 font-bold text-center text-white relative w-1/2">
+      <h1 className="sm:text-3xl md:text-2xl lg:text-5xl m-6 font-bold text-center text-white relative w-1/3">
         Experienced software engineer specialising in{" "}
         <strong className="font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
           Typescript
