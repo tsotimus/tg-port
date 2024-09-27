a354ed84e66af4b3908ff1efb64c2c17880438bf
Tsotne Gvadzabia <tsotne24@hotmail.com>
2024-09-27 23:46:40 +0100

    add speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Providers>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </Providers>
     </div>
   );
