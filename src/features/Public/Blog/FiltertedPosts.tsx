483226b7228fc30158767734cf93e0f41f48ab4f
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-02-10 16:54:42 +0000

    remove search on blog posts

--- src/features/Public/Blog/FiltertedPosts.tsx
+++ src/features/Public/Blog/FiltertedPosts.tsx
@@ -14,15 +14,15 @@ type FilteredPostsProps = {
 }
 
 const FilteredPosts = ({posts}: FilteredPostsProps) => {
-  const [searchValue, setSearchValue] = useState('')
+  // const [searchValue, setSearchValue] = useState('')
 
-  const filteredPosts = posts.filter((post) =>
-    post.title.toLowerCase().includes(searchValue.toLowerCase())
-  )
+  // const filteredPosts = posts.filter((post) =>
+  //   post.title.toLowerCase().includes(searchValue.toLowerCase())
+  // )
 
   return (
     <>
-      <div className='relative mb-8'>
+      {/* <div className='relative mb-8'>
         <Input
           type='text'
           value={searchValue}
@@ -37,11 +37,11 @@ const FilteredPosts = ({posts}: FilteredPostsProps) => {
         <Label htmlFor='search'>
           <SearchIcon className='absolute left-4 top-1/2 size-5 -translate-y-1/2' />
         </Label>
-      </div>
-      {filteredPosts.length === 0 && posts.length !==0 ? (
+      </div> */}
+      {posts.length === 0 && posts.length !==0 ? (
         <div className='my-24 text-center text-xl'>No posts found</div>
       ) : null}
-      <BlogGrid posts={filteredPosts}/>
+      <BlogGrid posts={posts}/>
     </>
   )
 }
