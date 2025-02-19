2d236b154fccd59a8862c6cb6edf1093c81c1b5d
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-02-19 15:03:16 +0000

    redirect away from vercel

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,30 @@ import {type  NextConfig } from "next";
 
 const generateConfig = async () => {
   const IS_UNDER_CONSTRUCTION = process.env.IS_UNDER_CONSTRUCTION === "true";
-
+  const IS_PROD = process.env.NODE_ENV === "production"
 
   const REDIRECTS = [
     {
       source: "/admin/tags",
       destination: "/admin/tags/0",
       permanent: true,
-    }
+    },
   ]
+
+  const PROD_REDIRECTS = IS_PROD ? [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          // key: "",
+          value: '.*\\.vercel\\.app$',
+        },
+      ],
+      destination: `${process.env.NEXT_PUBLIC_URL}/:path*`, 
+      permanent: true,
+    },
+  ] : []
 
   const nextConfig: NextConfig = {
     transpilePackages: ["@mdxeditor/editor", "geist"],
@@ -22,13 +37,15 @@ const generateConfig = async () => {
     redirects: async () => {
       return IS_UNDER_CONSTRUCTION
         ? [
+          ...REDIRECTS,
+          ...PROD_REDIRECTS,
             {
               source: "/((?!construction).)*",
               destination: "/construction",
               permanent: false,
             },
           ]
-        : [...REDIRECTS];
+        : [...REDIRECTS, ...PROD_REDIRECTS];
     },
     async rewrites() {
       return [
